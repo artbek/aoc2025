50e4f0faacb7de42b37ce552a796b2100e40027d
artbek <bart@artbek.co.uk>
2025-12-01 22:44:28 +0000

    [day01]: part 1 + part 2.

---
+++ day01/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#define dump(x) \
+		std::cout << #x << "=" << x << "\n";
+
+#define dumpv(x) \
+		std::cout << #x << ": "; \
+		for (auto el : x) std::cout << el << " "; \
+		std::cout << "\n";
+
+#define dumpm(x) \
+		std::cout << #x << ": "; \
+		for (auto [k,v] : x) std::cout << "[" << k << "]: " << v << ", "; \
+		std::cout << "\n";
+
+using ll = long long;
+
+const int MOD = 100;
+
+
+void part1()
+{
+	int counter = 0;
+
+	int state = 50;
+
+	char op;
+	int num;
+
+	while (std::cin >> op) {
+		std::cin >> num;
+
+		if (op == 'L') state -= num;
+		else state += num;
+
+		state %= 100;
+		if (state < 0) state += 100;
+		if (state == 0) counter++;
+	}
+
+	dump(counter);
+}
+
+
+// 2426 - too low
+// 324325 - too high
+// 5923 - correct
+void part2()
+{
+	int counter = 0;
+
+	int state = 50;
+
+	char op;
+	int num;
+
+	while (std::cin >> op) {
+		std::cin >> num;
+
+		int prev_state = state;
+
+		if (op == 'L') state -= num;
+		else state += num;
+
+		if (prev_state < 0  && state > 0) counter++;
+		if (prev_state > 0  && state < 0) counter++;
+
+		bool modified = false;
+
+		if (state > MOD) {
+			counter += (state / MOD);
+			modified = true;
+		} else if (state < -MOD) {
+			counter += (state / -MOD);
+			modified = true;
+		}
+
+		state %= 100;
+		if (state < 0) state += 100;
+
+		if (! modified && state == 0) counter++;
+	}
+
+	dump(counter);
+}
+
+
+int main()
+{
+	//part1();
+	part2();
+	return 0;
+}
